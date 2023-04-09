2c518902f62ceba07f4c162554a73b90610d8e59
Michael Muzzarelli <muzzarellimj@outlook.com>
2023-04-08 21:39:49 -0400

    Create Resource and ResourceSet interfaces

---
+++ app/src/main/kotlin/com/example/jocasta/data/model/Resource.kt
@@ -0,0 +1,11 @@
+package com.example.jocasta.data.model
+
+/**
+ * Resource interface to be implemented in named resource data classes - e.g., [Person].
+ */
+interface Resource
+
+/**
+ * Resource interface to be implemented in named resource set data classes - e.g. [PersonSet].
+ */
+interface ResourceSet
